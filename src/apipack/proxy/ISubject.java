916d9e93169299a194a662f7174a1a8bbb26513f
zhangdongxiao <zdongxiao@foxmail.com>
2018-09-12 17:41:59 +0800

    xinzegn

---
+++ src/apipack/proxy/ISubject.java
@@ -0,0 +1,13 @@
+package apipack.proxy;
+
+/**
+ * @author zhangdongxiao
+ * @create 2018-09-12 下午3:45
+ * @desc proxy接口
+ **/
+public interface ISubject {
+
+    public void hello(String str);
+    public void rent();
+
+}
