00870a79e858760678bc54393992c5d7f176b0ea
zhangdongxiao <zdongxiao@foxmail.com>
2019-06-17 18:02:35 +0800

    no message

--- src/apipack/javabean/maintest.java
+++ src/apipack/javabean/maintest.java
@@ -3,6 +3,10 @@ package apipack.javabean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * apipack.javabean
  *
@@ -13,8 +17,25 @@ public class maintest {
 
     public static void main(String[] args) {
 
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        SimpleDateFormat sdf0 = new SimpleDateFormat("mmss");
+        Date date = new Date();
+
+        String dataStmp = sdf0.format(date);
+
+        System.out.println("time it is " + dataStmp);
+        System.out.println("time stmp it is " + dataStmp.substring(1));
+
+        if (dataStmp.substring(1).equals("1")) {
+            System.out.println(111111);
+        } else {
+            System.out.println(222222);
+        }
+
+        int i = Integer.valueOf(dataStmp.substring(1));
+
+        System.out.println("mod = " + i % 500 );
 
     }
 
