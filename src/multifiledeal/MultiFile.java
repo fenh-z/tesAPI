8746d3949da209f0c7971c3fbcbbcf3f5ab982e0
zdongxiao <zdongxiao@foxmail.com>
2019-12-13 11:12:25 +0800

    d

---
+++ src/multifiledeal/MultiFile.java
@@ -0,0 +1,85 @@
+package multifiledeal;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author zhangdx
+ * @Parameter
+ * @CreateDate 2019/12/12 5:54 下午
+ * @Describe
+ */
+public class MultiFile {
+
+    static List<File> files = new ArrayList<>();
+    static String dealDir = "/Users/dxz/Public/ProgramCodes/file/multi/";
+
+    public static void main(String[] args) {
+
+        File dir = new File(dealDir);
+
+        File[] dirFile = dir.listFiles();
+
+        for (File file : dirFile) {
+
+            System.out.println("1111");
+
+            RandomAccessFile rf = null;
+            FileChannel fc = null;
+            FileLock lock = null;
+            try {
+                rf = new RandomAccessFile(file, "rw");
+                fc = rf.getChannel();
+
+                lock = fc.tryLock();
+
+                System.out.println("22222");
+                files.add(file);
+                System.out.println("33333");
+                Thread.sleep(1000);
+
+                lock.release();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if (rf != null) {
+                    try {
+                        rf.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fc != null) {
+                    try {
+                        fc.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (lock != null && lock.isValid()) {
+                    try {
+                        lock.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+        files.forEach(file -> System.out.println("!!!!!" + file.getName()));
+
+    }
+
+}
